62a2dd0fbe63f0d20e8c1316f2ca0dbfb78541a6
dralafandy <165874834+dralafandy@users.noreply.github.com>
2025-10-17 00:45:23 +0300

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,10 @@
+"""
+Cura Clinic Database Module
+نموذج قاعدة البيانات لنظام إدارة العيادة
+"""
 
+from .models import db, Database
+from .crud import crud, CRUDOperations
+
+__all__ = ['db', 'Database', 'crud', 'CRUDOperations']
+__version__ = '1.0.0'
